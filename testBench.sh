0e50e47e1a8db54e0ead245bd31fddea8aaf780c
Kyle Storey <kyle.r.storey@gmail.com>
2017-10-06 13:17:59 -0600

    Attempt to fix jpf instalation to be non-dependent (May not work)

--- testBench.sh
+++ testBench.sh
@@ -4,8 +4,10 @@
 #searches for source code, compiles and creates .jpf
 #it then times each of the classes nativly and in the datarace detector
 
-PathToHJlib='HJLibFiles/lib/hj-lib-byu.jar'
+Pathlib='HJLibFiles/lib'
 PathTORunJPF="HJLibFiles/lib/RunJPF.jar"
+PathClass='Classes:'$Pathlib'/hj-lib-byu.jar:'$Pathlib'/hamcrest-core-1.3.jar:'$Pathlib'/junit-4.12.jar'
+echo $PathClass
 PathToClasses='Classes'
 mkdir $PathToClasses
 
@@ -13,10 +15,10 @@ while fileOfClassNames='' read -r name || [[ -n "$name" ]]; do
     FoundJava=$(find -name "$name.java" -print -quit)
     if [ ! -f $PathToClasses/$name.class ]; then
       echo "Compliling: $name"
-      javac -cp $PathToClasses:$PathToHJlib -d $PathToClasses $FoundJava
+      javac -cp $PathClass -d $PathToClasses $FoundJava
       echo "Making $name.jpf"
       echo target=$name > $PathToClasses/$name.jpf
-      echo classpath=$PathToClasses:$PathToHJlib >> $PathToClasses/$name.jpf
+      echo classpath=$PathClass >> $PathToClasses/$name.jpf
       echo sourcepath=$(dirname $FoundJava) >> $PathToClasses/$name.jpf
 
     fi
@@ -29,7 +31,7 @@ while fileOfClassNames='' read -r name || [[ -n "$name" ]]; do
     echo '<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>'
     echo "Testing: $name"
     echo ""
-    /usr/bin/time -f "Native Running Time: %e seconds" java -cp $PathToClasses:$PathToHJlib $name
+    /usr/bin/time -f "Native Running Time: %e seconds" java -cp $PathClass $name
     echo "----------------------------------------------"
     /usr/bin/time -f "Test Running Time: %e seconds" java -jar $PathTORunJPF $PathToClasses/$name.jpf
 done < classNames.txt
