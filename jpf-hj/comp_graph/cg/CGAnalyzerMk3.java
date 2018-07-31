6b2c70368214210317823a2f8af591adc474f720
Joshua <washa3@gmail.com>
2018-07-31 07:11:24 -0600

    Stuff added for helper functions

--- jpf-hj/comp_graph/cg/CGAnalyzerMk3.java
+++ jpf-hj/comp_graph/cg/CGAnalyzerMk3.java
@@ -6,10 +6,12 @@ import static cg.Edges.*;
 import org.jgrapht.traverse.DepthFirstIterator;
 import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
+import java.copy.*;
 
 public class PocketAnalyzer {
     private static boolean race = false;
     private static boolean searchFinished = false;
+    private static DirectedAcyclicGraph<Node, DefaultEdge> graph;
 
     private class Bag extends HashSet<Pocket> {
 
@@ -19,37 +21,54 @@ public class PocketAnalyzer {
     public class Pocket extends HashSet<Node> {
         private final boolean direction; // up is false, down is true
         private boolean zipped;
-        private HashSet<Nodes> sAfter;
+        private HashSet<Node> sAfter;
         
         public boolean getDirection() {
             return direction;
-        }
+        }DirectedAcyclicGraph<Node, DefaultEdge> graph
 
-        public boolean getZipped() {
+        public boolean getZipped() DirectedAcyclicGraph<Node, DefaultEdge> graph{
             return zipped;
         }
 
         public void setZipped(boolean zipped) {
             this.zipped = zipped;
         }
+
+        public void updateZip(Node n)
+        {
+            if(!this.zipped && this.sAfter.contains(n))
+            {
+                this.zipped = true;
+            }
+        }
     }
 
-    Bag recursiveAnalyze(Node n, Bag pBag, List<Node> seriesNodes) {
+    public static boolean analyzeGraphForDataRace(DirectedAcyclicGraph<Node, DefaultEdge> graph) {
+        PocketAnalyzer.race = false;
+        PocketAnalyzer.searchFinished = false;
+        PocketAnalyzer.graph = graph;
+        Node start = getRootOf(graph);
+        Bag pBag = new Bag();
+        Bag result = recursiveAnalyze(start, pBag, new ArrayList<Node>());
+        return false;
+    }
+
+    private static Bag recursiveAnalyze(Node n, Bag pBag, List<Node> seriesNodes) {
         private static final boolean DOWN = true;
         private static final boolean UP = false;
-        
         Bag sBag = new Bag();
 
         if (n.isAsync()) {
             Bag asyncBag = deepcopy(pBag);
             for (child : n.getChildren()) {
                 prepForDownCheck(asyncBag);
-                Bag seriesResult = recursiveAnylize(child, asyncBag, new ArrayList<Node>());
+                Bag seriesResult = recursiveAnalyze(child, asyncBag, new ArrayList<Node>());
                 sBag = union(sBag, seriesResult); 
                 asnycBag = union(asyncBag, seriesResult);
             }
             n.setReadyForJoin(true);
-            Bag joinResult = recursiveAnylize(getJoin(n), pBag, new ArrayList<Node>());
+            Bag joinResult = recursiveAnalyze(getJoin(n), pBag, new ArrayList<Node>());
             sBag = union(sBag, joinResult);
             return sBag;
         }
@@ -69,8 +88,8 @@ public class PocketAnalyzer {
                 Set<Node> seriesAfter = new HashSet();
                 seriesAfter.add(i);
                 seriesAfter.add(getJoin(i));
-                newPocket = new Pocket(seriesNodes, DOWN, seriesAfter);
-                sBag.add(newPocket);
+                newPociteratoret = new Pocket(seriesNodes, DOWN, seriesAfter);
+                sBag.aiteratord(newPocket);
             }
         }
         
@@ -86,7 +105,7 @@ public class PocketAnalyzer {
                 Set<Node> seriesAfter = new HashSet();
                 seriesAfter.add(i);
                 seriesAfter.add(getAsync(i));
-                newPocket = new Pocket(seriesNodes, UP, seriesAfter);
+                newPocket = new Pocket(seriesNodes, UP, seriesAfter);        //TODO: Does the new bag's openPocket need to true or false);
                 sBag.add(newPocket);
             }
         }
@@ -94,10 +113,10 @@ public class PocketAnalyzer {
         return union(sBag, resultBag);
     }
 
-    boolean checkForDataRace(Bag pBag, Node n) {
+    private static boolean checkForDataRace(Bag pBag, Node n) {
         boolean dataRace = false;
         for (Pocket pocket : pBag) {
-            updateZip(pocket);
+            pocket.updateZip(n);
             if (pocket.isZipped()) continue;
             for (Node p : pocket) {
                 if (checkForConflicts(n, p)) {
@@ -108,17 +127,24 @@ public class PocketAnalyzer {
         return dataRace;
     }
 
-    Node getChild(None n) {
+    private static Node getChild(None n) {
         if (getChildren(n).size() == 1) {
             return n.getChildren(n).iterator().next();
         }
     }
 
-    Set<Node> getChildren(Node n) {
-
+    private static Set<Node> getChildren(Node n) {
+        Set<DefaultEdge> outgoing = graph.outgoingEdgesOf(n);
+        Set<Node> children = new HashSet<Node>();
+        Node edgeSource;
+        for(DefaultEdge e : outgoing)
+        {
+            child = graph.getEdgeTarget(edge);
+            children.add(child);
+        }
     }
 
-    void prepForDownCheck(Bag pBag) {
+    private static void prepForDownCheck(Bag pBag) {
         for (Pocket pocket : pBag) {
             if (pocket.dirrection == DOWN) {
                 pocket.setZipped(false);
@@ -140,20 +166,14 @@ public class PocketAnalyzer {
         }
     }
 
-    Bag union(Bag ... bags) {
-        Bag result = new Bag();
-        for (Bag bag : bags) {
-            for (Pocket pocket : bag) {
-                result.add(pocket);
-            }
+    Bag union(Bag... bags)
+    {
+        Bag union = new Bag();
+        ArrayList<Pockets> uPockets = union.getPockets();
+        for(Bag b : bags)
+        {
+            uPockets.addAll(b.getPockets);
         }
-        return result;
+        return union;
     }
-
-    void updateZip(Pocket pocket, Node n) {
-        if (pocket.seriesAfter.contains(n)) {
-            pocket.zip();
-        }
-    }
-
 }
