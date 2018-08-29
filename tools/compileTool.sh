f87f80c3a8b2026b7f889b6f08c53f0f18005fc8
Benjamin Ogles <benjaminogles@gmail.com>
2018-08-29 15:45:53 -0600

    Compile each tool

--- tools/compileTool.sh
+++ tools/compileTool.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-die_with_msg() {
-  echo "$1"
-  exit 1
-}
-
-[[ $# -eq 1 ]] || die_with_msg "Usage: compileTool.sh [cg|fasttrack|spbags]"
-
 JARS="lib"
 [[ -d "$JARS" ]] || die_with_msg "Could not find lib directory"
 
 JAVAC_CP="jpf-hj/src:$JARS/jpf.jar:$JARS/jpf-hj.jar:$JARS/hj-lib-byu.jar:$JARS/jgrapht-ext-0.9.1-uber.jar"
 echo "$JAVAC_CP"
 DEST="build"
-SOURCE_FILES=`find jpf-hj/src/extensions/$1 jpf-hj/src/extensions/util -type f -name "*.java"`
+SOURCE_FILES=`find jpf-hj/src/extensions/cg jpf-hj/src/extensions/fasttrack jpf-hj/src/extensions/spbags jpf-hj/src/extensions/util -type f -name "*.java"`
 
 if [[ ! -d "$DEST" ]]
 then
