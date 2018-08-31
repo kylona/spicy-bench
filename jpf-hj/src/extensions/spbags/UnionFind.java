ebc3e6f9ab518022b850bdce1b0bc289a8aae925
Benjamin Ogles <benjaminogles@gmail.com>
2018-08-31 16:07:27 -0600

    Simple implementation of UnionFind

--- jpf-hj/src/extensions/spbags/UnionFind.java
+++ jpf-hj/src/extensions/spbags/UnionFind.java
@@ -1,5 +1,28 @@
 package extensions.spbags;
 
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Simple implementation of the union find data structure
+ */
 public class UnionFind {
-  //TODO
+  Map<Integer, Integer> parent = new HashMap<>();
+
+  public void makeSet(int i) {
+    parent.put(i, i);
+  }
+
+  public int find(int i) {
+    if (parent.get(i) == i) {
+      return i;
+    }
+    int result = find(parent.get(i));
+    parent.put(i, result);
+    return result;
+  }
+
+  public void union(int i, int j) {
+    parent.put(j, i);
+  }
 }
