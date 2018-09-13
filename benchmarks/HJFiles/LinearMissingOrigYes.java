aeb7ca90b9ebe2e9e06904c9ca679eb76fb45b26
Joshua <washa3@gmail.com>
2018-09-13 15:43:45 -0600

    translations

---
+++ benchmarks/HJFiles/LinearMissingOrigYes.java
@@ -0,0 +1,47 @@
+ /*
+A translation of: antidep1-orig-yes.c
+Originally produced at the Lawrence Livermore National Laboratory
+Written by Chunhua Liao, Pei-Hung Lin, Joshua Asplund,
+Markus Schordan, and Ian Karlin
+
+Translated at Brigham Young University by Joshua Hooker
+
+*/
+import static edu.rice.hj.Module2.launchHabaneroApp;
+import static edu.rice.hj.Module2.forAll;
+import edu.rice.hj.api.*;
+
+public class LinearMissingOrigYes {
+  static int i,j = 0;
+  static int len = 100;
+  static double[] a,b,c;
+  public static void main(String[] args) throws SuspendableException {
+    a = new double[len];
+    b = new double[len];
+    c = new double[len];
+
+    for (i=0;i<len;i++)
+    {
+      a[i]=((double)i)/2.0; 
+      b[i]=((double)i)/3.0; 
+      c[i]=((double)i)/7.0; 
+    }
+      launchHabaneroApp(new HjSuspendable() {
+
+          @Override
+          public void run() throws SuspendableException {
+
+            forAll(0, len-1, new HjSuspendingProcedure<Integer>() {
+              public void apply(Integer i) throws SuspendableException {
+                c[j] += a[i] * b[i];
+                j++;
+              }
+            });
+
+            System.out.println("c[50]=" + c[50]);
+          }
+
+    });
+  }
+
+}
