3afaf1587a5b3ab9a9f5021529483b108e04d9fb
Joshua <washa3@gmail.com>
2018-09-13 15:56:32 -0600

    moar

---
+++ benchmarks/HJFiles/MinusMinusOrigYes.java
@@ -0,0 +1,52 @@
+ 
+/*
+A translation of: minusminus--orig-yes.c
+Originally produced at the Lawrence Livermore National Laboratory
+Written by Chunhua Liao, Pei-Hung Lin, Joshua Asplund,
+Markus Schordan, and Ian Karlin
+
+Translated at Brigham Young University by Joshua Hooker
+
+*/
+import static edu.rice.hj.Module2.launchHabaneroApp;
+import static edu.rice.hj.Module2.forAll;
+import edu.rice.hj.api.*;
+
+public class MinusMinusOrigYes {
+  static int i;
+  static int len = 100;
+  static int numNodes;
+  static int numNodes2;
+  static int[] x;
+  public static void main(String[] args) throws SuspendableException {
+
+    numNodes = len;
+    numNodes2 = 0;
+    x = new int[100];
+    
+      // initialize x[]
+    for (i=0; i< len; i++)
+    {
+        if (i%2==0)
+            x[i]=5;
+        else
+            x[i]= -5;
+    }
+
+      launchHabaneroApp(new HjSuspendable() {
+
+          @Override
+          public void run() throws SuspendableException {
+
+            forAll(0, numNodes-1, new HjSuspendingProcedure<Integer>() {
+              public void apply(Integer i) throws SuspendableException {
+                if(x[i]<=0)
+                    numNodes2--;
+              }
+            });
+          }
+    });
+    System.out.printf("numNodes2 = %d\n", numNodes2);
+  }
+
+}
